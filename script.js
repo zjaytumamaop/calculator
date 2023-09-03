3c50cb5d7f98c15faf54dffae540593bdebb05f1
Z-Jay <zjaytumamaop@gmail.com>
2023-09-02 23:45:18 -0400

    started a result button to be clickable

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let first = 1;
 let operator = "add";
 
 let second = 3;
+
+let sum = 23;
 function add(a,b){
     return a +b;
 }
@@ -18,7 +20,10 @@ function divide(a,b){
 }
 
 function operate(){
-  return (window[operator](first,second));
+   const result = document.getElementById('result');
+   result.innerHTML =  (window[operator](first,second));
 }
 
 console.log(operate());
+
+document.getElementById('=').addEventListener('mousedown', operate());
