79500b4e5201f9cee50156a28110135393aa05db
Z-Jay <zjaytumamaop@gmail.com>
2023-09-07 23:26:05 -0400

    added operators to be clickable and AC button'

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //javascript 
-let first = 13;
+let first = 0;
 
-let operator = "add";
+let operator = "";
 
 let second = 3;
 
@@ -10,10 +10,11 @@ let currentNum = 0;
 let displayNum = 0;
 
 function add(a,b){
-    return a +b;
+    return a + b;
 }
 function subtract(a,b){
     return a - b;
+
 }
 function multiply(a,b){
     return a*b;
@@ -22,7 +23,10 @@ function divide(a,b){
     return a/b;
 }
 
-function operate(operator, first, second) {
+function operate(operator, first) {
+    second = displayNum;
+    displayNum = 0;
+    
     const resultElement = document.querySelector('.result');
     displayNum = window[operator](first, second); // Assuming operator is a valid function in the window object.
     resultElement.innerHTML = displayNum; // Display the result in the 'result' element.
@@ -41,6 +45,22 @@ function updateDisplayNum(){
     resultElement.innerHTML = displayNum;
 }
 
+function chooseOperator(op){
+    operator = op;
+
+    first = displayNum;
+    displayNum = 0;
+    console.log(first);
+    console.log(operator);
+    console.log(displayNum);
+    updateDisplayNum();
+}
+
+function clear (){
+    displayNum = 0;
+    updateDisplayNum();
+}
+
 //console.log(operate());
 const numbers = document.querySelectorAll('.num');
 numbers.forEach(function (number) {
@@ -49,6 +69,19 @@ numbers.forEach(function (number) {
 });
 
 
+const mult = document.getElementById('X');
+mult.addEventListener('click', chooseOperator('multiply'));
 
+const divi = document.getElementById('/');
+divi.addEventListener('click',chooseOperator('divide'));
 
-document.getElementById('=').addEventListener('click', () => operate(operator,first,second));
+const ad = document.getElementById('+');
+ad.addEventListener('click',chooseOperator('add'));
+
+const sub = document.getElementById('-');
+sub.addEventListener('click',chooseOperator('subtract'));
+
+const ac = document.getElementById('AC');
+ac.addEventListener('click',clear)
+
+document.getElementById('=').addEventListener('click', () => operate(operator,first));
